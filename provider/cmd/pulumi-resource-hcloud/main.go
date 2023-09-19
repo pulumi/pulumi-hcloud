7a4bd2fb0862e823fa8294825bf436c04d3f8417
Guinevere Saenger <guinevere@pulumi.com>
2023-09-19 15:18:41 -0700

    migrate to schema-embed.json

--- provider/cmd/pulumi-resource-hcloud/main.go
+++ provider/cmd/pulumi-resource-hcloud/main.go
@@ -17,11 +17,14 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
-
+	_ "embed"
 	hcloud "github.com/pulumi/pulumi-hcloud/provider"
 	"github.com/pulumi/pulumi-hcloud/provider/pkg/version"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
+
+//go:embed schema-embed.json
+var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
